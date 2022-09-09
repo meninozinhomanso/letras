0d9a03595dcc008d5acfc5e1bb4c094f33d97146
meninozinhomanso <39411695+meninozinhomanso@users.noreply.github.com>
2022-09-09 14:32:09 -0300

    Update and rename frasestoppepeca.js to frasestoppeca.js

--- frasestoppeca.js
+++ frasestoppeca.js
@@ -12,7 +12,7 @@ var powerThoughts = [
 
 var content = document.getElementById('content');
 (function() {
-	content.innerHTML= powerThoughts[Math.floor((Math.random() * 11196) + 1)];
+	content.innerHTML= powerThoughts[Math.floor((Math.random() * 96) + 1)];
 	var totalCount = 52;
     var num = Math.floor((Math.random() * totalCount) + 1);
     document.body.background = 'background/back' + num + '';
