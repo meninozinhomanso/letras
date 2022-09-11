578e48d23966f27ce60a77143435cd32fb21d2d9
meninozinhomanso <39411695+meninozinhomanso@users.noreply.github.com>
2022-09-10 22:23:22 -0300

    Update and rename frasestop10mm9.js to frasestop10mm10.js

--- frasestop10mm10.js
+++ frasestop10mm10.js
@@ -1149,7 +1149,7 @@ var powerThoughts = [
 ];
 var content = document.getElementById('content');
 (function() {
-	content.innerHTML= powerThoughts[Math.floor((Math.random() * 1157) + 1)];
+	content.innerHTML= powerThoughts[Math.floor((Math.random() * 1146) + 1)];
 	var totalCount = 52;
     var num = Math.floor((Math.random() * totalCount) + 1);
     document.body.background = 'background/back' + num + '.jpg';
